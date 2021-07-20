f542549049f5acdf394a52fdefe307dd7e607e91
要贝贝 <yaobb@dingdao.com>
2021-07-20 16:03:50 +0800

    2

--- service-worker.js
+++ service-worker.js
@@ -3,16 +3,8 @@ importScripts("/pwa-1/precache-manifest.80060960360ef482a5c3b70ccff2a30f.js", "h
 // 设置相应缓存的名字的前缀和后缀
 workbox.core.setCacheNameDetails({
     prefix: 'pdf-image-vue-cache',
-    suffix: 'v1.0.0',
+    suffix: 'v1.1.1',
 })
-// 让我们的service worker尽快的得到更新和获取页面的控制权
-workbox.core.skipWaiting();
-workbox.core.clientsClaim();
-
-/* vue-cli3.0通过workbox-webpack-plagin 来实现相关功能，我们需要加入
-* 以下语句来获取预缓存列表和预缓存他们，也就是打包项目后生产的html，js，css等\* 静态文件
-*/
-workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
 // 缓存web的css资源
 workbox.routing.registerRoute(
     // Cache CSS files
@@ -66,7 +58,7 @@ workbox.routing.registerRoute(
     })
 );
 
-// 缓存get api请求的数据
+//缓存get api请求的数据
 workbox.routing.registerRoute(
     new RegExp(/.*/),
     workbox.strategies.networkFirst({
