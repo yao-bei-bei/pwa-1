487a50753f3b8d927721f69d3904541fb988b4d2
要贝贝 <yaobb@dingdao.com>
2021-08-25 18:49:38 +0800

    1

--- manifest/service-worker.js
+++ manifest/service-worker.js
@@ -2,6 +2,7 @@
 // workbox 3.x 开始是将 workbox 核心 lib 放在 CDN 维护
 // 当然也可以挪到自己的 CDN 维护
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0-alpha.3/workbox-sw.js');
+console.log(workbox)
 if (workbox) {
     console.log(`Yay! workbox is loaded 🎉`);
     // 设置相应缓存的名字的前缀和后缀
